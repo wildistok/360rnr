56bfbaf0dc1f701a24954512708bf9b3ebe2bbcf
Irina Stokolyas <istokolyas@solanteq.com>
2020-01-20 23:54:16 +0300

    Question for IISAEV: How todo coroutines here?

---
+++ src/test/kotlin/PanoCoroutinesBanchmarkTest.kt
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test
+import java.awt.image.BufferedImage
+import java.io.File
+import java.io.IOException
+import javax.imageio.ImageIO
+
+class PanoCoroutinesBanchmarkTest {
+
+    @Test
+    fun Test1() {
+        var pano = PanoCoroutines()
+        var res = pano.panoWritter("8k_2.jpg", "8k_1.jpg", "/Users/istokolyas/Desktop/360FishEye/src/test/resources")
+
+        saveImg(res, "jpg", "/Users/istokolyas/Desktop/360FishEye/src/test/resources/multi_thread_pano.jpg" )
+    }
+
+    fun saveImg(img: BufferedImage, format: String, name: String) {
+        try {
+            ImageIO.write(img, format, File(name))
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+    }
+}
